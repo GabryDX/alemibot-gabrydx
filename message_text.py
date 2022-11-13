14d54e264f8f410abc526a12b32eb3e8fa9a8918
GabryDX <gabrydx@hotmail.com>
2022-11-13 23:06:23 +0100

    Test update 2

--- message_text.py
+++ message_text.py
@@ -51,7 +51,7 @@ async def transcribe2_cmd(client: alemiBot, message: Message):
 	elif message.voice or message.audio or (message.document and message.command["whatsapp"]):
 		path = await client.download_media(message)
 	else:
-		return await edit_or_reply(message, str(message.media))
+		return await edit_or_reply(message, lang + " " + str(message.media))
 		# return await edit_or_reply(message, "`[!] → ` No audio given")
 	if message.command["whatsapp"]:
 		try:
