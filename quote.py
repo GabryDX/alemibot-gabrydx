3bcd443c44ff69d4ababe507420e348609240be5
GabryDX <gabrydx@hotmail.com>
2022-11-13 23:57:12 +0100

    Added new quote call

--- quote.py
+++ quote.py
@@ -1,4 +1,5 @@
 import inspirobot
+import requests
 
 from alemibot import alemiBot
 
@@ -15,11 +16,11 @@ INTERRUPT = False
 
 
 @HELP.add(sudo=False)
-@alemiBot.on_message(is_allowed & filterCommand("quote"))
+@alemiBot.on_message(is_allowed & filterCommand("quote_img"))
 @report_error(logger)
 @set_offline
 @cancel_chat_action
-async def quote_cmd(client, message):
+async def quote_img_cmd(client, message):
 	"""send stupid quote image
 
 	Code comes from https://github.com/SamHDev/inspiro.
@@ -35,3 +36,35 @@ async def quote_cmd(client, message):
 		await send_media(client, message.chat.id, quote.url, reply_to_message_id=reply_to)
 	except Exception as ex:
 		await edit_or_reply(message, "`[!] → ` " + str(ex))
+
+
+@HELP.add(sudo=False)
+@alemiBot.on_message(is_allowed & filterCommand("quote"))
+@report_error(logger)
+@set_offline
+@cancel_chat_action
+async def quote_cmd(client, message):
+	"""send stupid quote message
+
+	Code comes from https://geekflare.com/random-quote-python-code/ and
+	quotes come from https://quote-garden.herokuapp.com/api/v3/quotes/random.
+	"""
+	msg = message
+	reply_to = message.id
+	if message.reply_to_message is not None:
+		msg = message.reply_to_message
+	try:
+		# making the get request
+		response = requests.get("https://quote-garden.herokuapp.com/api/v3/quotes/random")
+		if response.status_code == 200:
+			# extracting the core data
+			json_data = response.json()
+			data = json_data['data']
+
+			# getting the quote from the data
+			quote = data[0]['quoteText']
+			await edit_or_reply(msg, quote)
+		else:
+			return await edit_or_reply(message, "`[!] → ` Error while getting quote")
+	except:
+		return await edit_or_reply(message, "`[!] → ` Something went wrong! Try Again!")
